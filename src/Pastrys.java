1a3ca1a46ed85ea939d83897278d2512baac1248
nelson <ralph_nelson25@milton.edu>
2024-10-29 22:30:38 -0400

    Finished HW 9/11

--- src/Pastrys.java
+++ src/Pastrys.java
@@ -1,5 +1,40 @@
 public class Pastrys {
     //homework
     // New class Pastry, variables type, flavor, number with "getter" and "setter" methods
+    private int numberOfPastries;
+    private String nameOfPastry;
+    private String flavor;
+
+    public void Pastry (int paramNumber0fPastries, String type, String name0fPastry){
+
+        numberOfPastries = paramNumber0fPastries;
+        this.nameOfPastry = name0fPastry;
+        this. flavor = type;
+
+        pastries();
+    }
+
+    public void pastries(){
+
+        numberOfPastries = 2;
+        flavor = "strawberry";
+        nameOfPastry = "donuts";
 
 }
+    public String getType () { return flavor; }
+        //public void type(String type) { this. flavor = flavor; }
+
+        public void printInfo(){
+        System.out.println("Your order comes with " + numberOfPastries + flavor + nameOfPastry);
+
+
+        }
+    public String getFlavor() {
+        return flavor;
+
+    }
+    public void setFlavor(String flavor ) {
+        this.flavor = flavor;
+    }
+}
+
